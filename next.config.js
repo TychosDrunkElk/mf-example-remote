9c280d540681e8ba6ea84f73dbe5fbe38c0dba3a
phoebe <phoebe@spotify.com>
2021-08-13 11:00:03 -0400

    add some things

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
-module.exports = {
+const { withFederatedSidecar } = require("@module-federation/nextjs-mf")
+
+module.exports = withFederatedSidecar({
+  name: 'exampleRemote',
+  filename: 'static/chunks/remoteEntry.js',
+  exposes: {
+    './BizBirds': './pages/index.js',
+  },
+  shared: {
+    react: {
+      requiredVersion: false,
+      singleton: true,
+    },
+  }
+})({
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.output.publicPath = 'auto';
+    return config;
+  }
+});
